70df666e35cbc9d6ccd14609129d8bb27a12fa12
saubuny <122068609+saubuny@users.noreply.github.com>
2023-12-15 11:52:13 -0500

    Draw a Triangle

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+// NOTE: Use Orthographic Projection
+
 // Callback to resive viewport on window resize
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -23,8 +25,9 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  // Required for MacOS
+#ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
   GLFWwindow *window =
       glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Terraria", NULL, NULL);
@@ -34,14 +37,94 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     printf("Failed to initialize GLAD\n");
     return -1;
   }
 
-  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // === Init Code for an Object ===
+
+  // TODO: Read in shaders from external files
+  const char *vertexShaderSource =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main() {\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\0";
+
+  // Create shader object
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  // Attach source code to object and compile
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  // Check for compiling success (TODO: Do this for frag shader also)
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("Error: Could not compile vertex shader\n%s\n", infoLog);
+  }
+
+  // Frag shader...
+  const char *fragmentShaderSource =
+      "#version 330 core\n"
+      "out vec4 FragColor;\n"
+      "void main() {\n"
+      "  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+      "}\0";
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  // Create shader program
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // Check for error
+  // 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  // if(!success) {
+  //     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+  //     ...
+  // }
+
+  // Vertices for our triangle
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
+  // Create VBO (an OpenGL Object) for storing vertex data on GPU
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  // The VAO, which houses most of the data in one convenient object
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  // Copy data into buffer, GL_STATIC_DRAW means we only set the data once but
+  // will use it many times
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Tell OpenGL how to interpret vertex data
+  // 0 -> layout (location = 0)
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
 
   // Main loop
   while (!glfwWindowShouldClose(window)) {
@@ -50,6 +133,10 @@ int main() {
     // === Rendering === //
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // === Update === //
     glfwPollEvents();
